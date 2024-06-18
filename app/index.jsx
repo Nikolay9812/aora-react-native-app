0632eafb3cfacfaf68b331bfd2cc27efe0863297
Nikolay9812 <73443601+Nikolay9812@users.noreply.github.com>
2024-06-18 20:51:34 +0200

    adding nativeWind conigurating app structure and creating TabBar

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Link, Slot } from 'expo-router'
+import { Text, View } from "react-native";
+import React from "react";
+import { Link, Slot } from "expo-router";
 
-export default function App  () {
-    return (
-        <View style={styles.container}>
-            <Text>Aora</Text>
-            <Link href='/profile' style={{color:'blue'}}>Go to profile</Link>
-      </View>
-  )
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora</Text>
+      <Link href="/home" style={{ color: "blue" }}>
+        Go to Home
+      </Link>
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor:'#fff',
-        alignItems:'center',
-        justifyContent:'center',
-    }
-})
